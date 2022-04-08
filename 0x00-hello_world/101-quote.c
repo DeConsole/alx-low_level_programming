44446f95c4811eb88f023dd6fd26aa91d8712bca
DeConsole <dennisconsole@gmail.com>
2022-04-07 19:33:21 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function where compiler starts
+ *
+ * Return: 1 end program
+ */
+int main(void)
+{
+	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fprintf(stderr, "%s\n", n);
+	
+	return (1);
+}
