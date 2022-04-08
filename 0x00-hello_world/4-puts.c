8acc3546d3e990f73e5bf2c3c3fbec198acceef0
DeConsole <dennisconsole@gmail.com>
2022-04-07 18:25:22 -0700

    Correcting 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  *main - start of compilation
  *
  *Return: 0 after printing the output
- * */
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
