e4e3beccc7df0e91ff37ba78b90f83e72a5f9023
DeConsole <dennisconsole@gmail.com>
2022-04-07 18:01:12 -0700

    Updating programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,8 @@
 /**
  *Standard input and output
  */
-
 int main(void)
-// Main function in C
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }
