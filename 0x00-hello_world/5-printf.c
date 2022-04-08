947a754ba33218046482d8b8b70047c93708144d
DeConsole <dennisconsole@gmail.com>
2022-04-07 18:34:32 -0700

    printf,with proper grammar, but the outcome is a piece of art

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Start of compilation
+ *
+ * Return: 0 null value returned after running
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
