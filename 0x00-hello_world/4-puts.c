b64e4222da2ce821d160d4c6091a24c083c81146
DeConsole <dennisconsole@gmail.com>
2022-04-07 18:12:29 -0700

    update of 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  *main - start of compilation
  *
- *return: 0 after printing the output
+ *Return: 0 after printing the output
  * */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
