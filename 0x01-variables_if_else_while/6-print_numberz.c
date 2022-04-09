49dcc869c47e8df512a0765cd1fb56742cfb11ba
DeConsole <dennisconsole@gmail.com>
2022-04-08 17:47:51 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main function from where the compiler starts from
+ *
+ * Return: 0 terminate at success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <=10; i++)
+{
+	putchar(i);
+}
+
+	putchar('\n');
+
+	return (0);
+
+}
