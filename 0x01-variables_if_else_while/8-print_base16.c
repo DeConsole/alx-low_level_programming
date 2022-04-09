6044e9c199408ced48de97f494047fc3d8c584ac
DeConsole <dennisconsole@gmail.com>
2022-04-08 18:45:52 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main function that starts
+ *
+ * Return: 0 terminates when successful
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+{
+	putchar((num % 10) + '0');
+}
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+{
+	putchar(letter);
+}
+
+	putchar('\n');
+
+	return (0);
+
+}
