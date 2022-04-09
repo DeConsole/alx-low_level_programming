aa08409f8180d992a98bcfb5af4530e063624fe6
DeConsole <dennisconsole@gmail.com>
2022-04-08 18:20:11 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main function where complier starts
+ *
+ * Return: 0 terminates when successful
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet = 'a'; alphabet++)
+{
+	putchar(alphabet);
+}
+
+	putchar('\n');
+
+	return(0);
+
+}
