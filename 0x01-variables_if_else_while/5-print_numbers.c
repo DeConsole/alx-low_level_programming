aef6828bc99b2e4fd62c067f3970181c0318f4dc
DeConsole <dennisconsole@gmail.com>
2022-04-08 17:39:00 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main function where the compiler starts from
+ *
+ * Return: 0 terminate when successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+{
+	printf("%i", i);
+}
+
+	printf("\n");
+
+	return (0);
+
+}
