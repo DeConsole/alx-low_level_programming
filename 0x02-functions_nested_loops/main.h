0ea363cdf3aa686fedf73f08e1cbf5baf67f30d4
DeConsole <dennisconsole@gmail.com>
2022-04-12 16:07:58 -0700

    updates

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-/**
- * File: main.h
- * Auth: Dennis Njoroge
- * Desc: header file
- */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
