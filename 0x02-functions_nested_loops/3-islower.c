61edf674655617804c09e62a9e0ccc06ea43b4b7
DeConsole <dennisconsole@gmail.com>
2022-04-12 17:50:49 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - Short description,single line
+ * @c: contains value to be compared
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+{
+	return (1);
+}
+
+	else
+{
+	return (0);
+}
+
+}
