0bfae8c3adfaf6cff7ca721f483d90baf478214a
DeConsole <dennisconsole@gmail.com>
2022-04-13 04:32:22 -0700

    changes to 4-description of c and 98 Battery Street, the OG

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
- * _isalpha - Return 1 if C is a letter
  *
- * Return: 0 Always
+ * _isalpha - Return 1 if C is a letter
+ * @c: the int to print
+ *
+ * Return: 0 - success
  */
 int _isalpha(int c)
 {
-
 	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
 {
 	return (1);
 }
-
 	else
 {
 	return (0);
 }
-
 }
