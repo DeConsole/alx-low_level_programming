37c6d0196e5452b4d2cb3a5b5db5d10ff934b740
DeConsole <dennisconsole@gmail.com>
2022-04-13 12:14:52 -0700

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - check for multiples of 3 nd 5
+ *
+ * Return: 0 - success
+ */
+int main(void)
+{
+int x = 1024;
+int y;
+int sum = 0;
+
+for (y = 0; y < x; y++)
+{
+if ((y % 3 == 0 || (y % 5 == 0))
+{
+sum = sum + y;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
