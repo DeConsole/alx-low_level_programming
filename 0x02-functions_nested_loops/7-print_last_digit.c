168bfa8c63e8a6c72a30c2ce4af2791eef4ed16c
DeConsole <dennisconsole@gmail.com>
2022-04-12 19:56:38 -0700

    corrections

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,7 @@
  */
 int print_last_digit(int n)
 {
-	
+
 	int last_digit;
 
 	if (n < 0)
@@ -16,7 +16,7 @@ int print_last_digit(int n)
 	_putchar (last_digit + '0');
 	return (last_digit);
 }
-	
+
 	else
 {
 	last_digit = (n % 10);
