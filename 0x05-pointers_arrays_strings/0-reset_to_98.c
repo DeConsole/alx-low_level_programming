60019d5e3a2655874e6b7506627569a141b6c462
DeConsole <dennisconsole@gmail.com>
2022-04-21 16:22:02 -0700

    Creating main.h and 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value *n points to.
+ * @n: the number to modify.
+ * Return: void to return null
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+These are c pointers and arrays
