01aadf12dae2133bb0615a065e8823eac978ab37
DeConsole <dennisconsole@gmail.com>
2022-04-21 16:49:00 -0700

    Creating 2-strlen.c - This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - calculate the length of a string
+ * @s: char that stores length
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+	
+	while (s[length] != '\0')
+{	
+	length++;
+}
+	return (length);
+
+}
