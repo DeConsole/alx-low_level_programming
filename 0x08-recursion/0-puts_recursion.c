aebe2f78c79038498a4a2ac4d86fb862722c6081
DeConsole <dennisconsole@gmail.com>
2022-04-28 19:16:52 -0700

    correcting 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,15 +2,16 @@
 /**
  * _puts_recursion - Prints a string followed by a new line
  * @s: string
- * Return: On success 1
- * On error, -1 is returned, and errno is set appropriately
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s == '\0')
 {
 	_putchar('\n');
+	return;
 }
+
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
