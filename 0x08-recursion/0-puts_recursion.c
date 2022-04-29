79383f734faac5a6aa5744c5c71728ad9ab1cfad
DeConsole <dennisconsole@gmail.com>
2022-04-28 19:40:54 -0700

    correcting 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
  * _puts_recursion - Prints a string followed by a new line
- * @s: string
+ * @s: pointer block of memory
  * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s == '\0')
 {
 	_putchar('\n');
+	return;
 }
 
-	_putchar(*s);
+	_putchar (*s);
 	_puts_recursion(s + 1);
 }
