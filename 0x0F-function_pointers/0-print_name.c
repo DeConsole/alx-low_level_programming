bee4f133358cc8dd36399264280d8f6065041f74
DeConsole <dennisconsole@gmail.com>
2022-05-16 19:25:21 -0700

    Creating function_pointers.h and 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - function that prints out a name
+ * @name: name that will be printed out
+ * @f: pointer to the printing function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+	return;
+	f(name);
+}
