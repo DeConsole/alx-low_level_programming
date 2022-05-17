f18f7aff894ce003ce9826d8b08a7de756eee982
DeConsole <dennisconsole@gmail.com>
2022-05-17 15:55:51 -0700

    creating variadic_functions.h and 0-sum_them_all.c - Beauty is variable, ugliness is constant

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - returns sum
+ * @n: amount of the arguments
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list thelist;
+	unsigned int d;
+
+	int sum = 0;
+
+	if (n == 0)
+	return (0);
+
+	va_start(thelist, n);
+
+	for (d = 0; d < n; d++)
+	sum += va_arg(thelist, int);
+	va_end(thelist);
+
+	return (sum);
+}
