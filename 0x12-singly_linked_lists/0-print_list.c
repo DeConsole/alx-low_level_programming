7ebd050e2f4d86742ae2b1ab223c5e4602bebe53
DeConsole <dennisconsole@gmail.com>
2022-05-24 14:09:29 -0700

    creating 1-list_len.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,10 +5,9 @@
  * Return: number of elements in the list.
  */
 size_t print_list(const list_t *h)
-{
-	size_t nelem;
-	nelem = 0;
+{	size_t nelem;
 
+	nelem = 0;
 	while (h != NULL)
 {
 	if (h->str == NULL)
