b3ec1838790fdb7ea96f23ef6fdb6ea15b6045f5
DeConsole <dennisconsole@gmail.com>
2022-05-24 15:01:15 -0700

    updating 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 /**
- * list_len - returns then number of elements in a list
+ * list_len - returns then number of 
+ * elements in a list
  * @h: singly linked list
- * Return: number of elements in the list.
+ * Return: number of elements in list
  */
 size_t list_len(const list_t *h)
 {
