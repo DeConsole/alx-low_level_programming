b1acc15c56feb5fed4de040900b350582319eec5
DeConsole <dennisconsole@gmail.com>
2022-05-27 12:44:16 -0700

    creating the first three first files README.md, lists.h and 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - prints elements of a linked list
+ * @h: linked list
+ * Return: nodes number
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+{
+	printf("%d\n", h->n);
+	num++;
+	h = h->next;
+}
+	return (num);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+this are more linked lists
