8f563dafd7bc9a484710430db94c71c7ef615e7d
DeConsole <dennisconsole@gmail.com>
2022-06-02 19:26:58 -0700

    updating 2-append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,19 +13,17 @@ int append_text_to_file(const char *filename, char *text_content)
 	return (-1);
 
 	file = open(filename, O_APPEND | O_RDWR, 0664);
-
 	if (file == -1)
 	return (-1);
 
 	if (text_content != NULL)
 {
-	for (i = 0; text_content[i]; i++);
+	for (i = 0; text_content[i]; i++)
+;
 	fwrite = write(file, text_content, i);
-
 	if (fwrite == -1)
 	return (-1);
 }
 	close(file);
-
 	return (1);
 }
