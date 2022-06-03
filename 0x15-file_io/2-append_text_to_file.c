9e603ef038d5997b8be494485137b8b8ddeb6da5
DeConsole <dennisconsole@gmail.com>
2022-06-02 18:57:33 -0700

    creating 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * create_file - creates a file
- * @filename: name of the file to create
- * @text_content: a NULL terminated string to write to the file
+ * append_text_to_file - appends text at end of a file
+ * @filename: name of the file
+ * @text_content: the NULL terminated string to add
  * Return: 1 on success, -1 on failure
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int file, fwrite, i;
 
 	if (filename == NULL)
 	return (-1);
 
-	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	file = open(filename, O_APPEND | O_RDWR, 0664);
 
 	if (file == -1)
 	return (-1);
@@ -22,9 +22,11 @@ int create_file(const char *filename, char *text_content)
 	for (i = 0; text_content[i]; i++)
 ;
 	fwrite = write(file, text_content, i);
+
 	if (fwrite == -1)
 	return (-1);
 }
 	close(file);
+
 	return (1);
 }
